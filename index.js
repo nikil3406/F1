22fd05e1b6603526687028114d49a439d8f55f2c
Nikil <nikilreddy3406@gmail.com>
2026-03-07 13:18:19 +0530

    Create directory for the new F1 website

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(port, () => {
+    console.log(`Port open at ${port}`);
+});

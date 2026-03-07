c028c80a884fe57984016363f75574d7640dfa40
Nikil <nikilreddy3406@gmail.com>
2026-03-07 13:52:54 +0530

    cmopleted dash-board

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+app.get("/drivers",(req,res)=>{
+    res.render("drivers.ejs");
+})
+
+app.get("/teams",(req,res)=>{
+    res.render("teams.ejs");
+})
+
+app.get("/schedule",(req,res)=>{
+    res.render("schedule.ejs");
+})
+
+app.get("/home",(req,res)=>{
+    res.redirect("/");
+})
 app.listen(port, () => {
     console.log(`Port open at ${port}`);
 });
